e647de6cf97d254ec5556f761439acf88ef609dd
sarah <sarah.michaux@hotmail.be>
2019-05-23 11:41:40 +0200

    add extra exercises

---
+++ exercise_6/myscript.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+      counter: 0
+    }, methods: {
+        increase: function(){
+          this.counter++;
+      }
+    }
+  });
---
+++ exercise_7/myscript.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+      counter: 0,
+      x: 0,
+      y: 0,
+    }, methods: {
+        increase: function(){
+          this.counter++;
+      },
+      updateCoordinates: function(event){
+        this.x = event.clientX;
+        this.y = event.clientY;
+        }
+    }
+  });
